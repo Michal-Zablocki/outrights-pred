2a8798edc3d8a57b0ac252cde4968543deab69bf
MZ <zablockimikael@gmail.com>
2025-11-09 11:03:23 +0100

    1.01

---
+++ TODO.py
@@ -0,0 +1,16 @@
+# TODO
+
+# 1. finish the readme
+# 2. add some lower rated teams to the regression
+# 3. calculate the whole probability x position matrix and visualize it
+# 4. add optional random tie breaking
+# 5. graph odds to win over each round
+
+# 6. get/scrape outrights odds [check the odds api]
+# 7. scrape opta power rankings
+
+# 8. test simple betting on single games strategies (e.g. bet p units on each game with prob of team A or B winning >= 4.0 in a given league/season(s))
+
+# european leagues with seasons not ending around May: (Finland, Ireland), Norway, Sweden
+# american leagues -||-: Brazil, Canada, Ecuador, USA, Uruguay
+# asian leagues -||-: Japan, Korea et al.
