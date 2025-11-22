0deccd0bd28f3527e63514d0013f001e88b747a7
MZ <zablockimikael@gmail.com>
2025-11-22 11:02:17 +0100

    1,12

--- TODO.py
+++ TODO.py
@@ -1,22 +1,28 @@
-# TODO
+###
 
-# 1. add docs and clean up the files/columns a bit
+# backtesting [high prio]
+
+# 1. write some script to "generate" own odds and compare them to the bookies' odds on single games; look up old kaggle stuff
+# 2. check how successful opta's and clubelo's simulations/predictitons were in the past
+# 3. run an analysis like in the paper - middle of season standings vs. end of season standings
+# 4. manual collection of data on historical outrights odds: source | date | season | round | league | bookie's odds for teams | our model's odds for teams
 
 ###
 
-# 2. calculate the whole probability x position matrix
+# the tool [medium prio]
+
+# 1. implement range 9-24 in run_multiple_sims;
+# 2. add WTB (winning tie-breakers) sim
+# 3. [low prio] visualization: conditional formatting of odds, graph odds over time
+# 4. additional factor E: every elo is multiplied by E (e.g. 1.2) to potentially decrease variance
+# 5. set HFA to 90 for international competitions
 
 ###
 
-# 4. graph odds to win over each round
+# scraping [low prio]
 
-###
-
-# 5. get/scrape outrights odds [check the odds api] OR parse the screenshots
-
-###
-
-# 6. scrape opta power rankings
+# 1. get/scrape outrights odds [check the odds api] OR parse the screenshots
+# 2. scrape opta power rankings
 
 ###
 
--- helpers.py
+++ helpers.py
@@ -22,8 +22,34 @@ K_FACTOR = 20  # Elo rating system K-factor
 NU = 1.5  # draw parameter
 
 
+def read_fixtures(
+    league_id: str, season: str, is_european_league: Optional[bool] = False
+) -> dict:
+    """Read fixtures from a local JSON file."""
+
+    with open(f"data/fixtures_api/fixtures_{league_id}_{season}.json", "r") as f:
+        fixtures = json.load(f)['response']
+        if is_european_league:
+            fixtures = [
+                fixture
+                for fixture in fixtures
+                if fixture['league']['round'].startswith('League Stage')
+            ]
+    return fixtures
+
+
+def save_round_divide(num, den, precision=2) -> int | float:
+    """Safe division with rounding. Returns -1 if division by zero."""
+
+    if den == 0:
+        return -1
+    else:
+        return round(num / den, precision)
+
+
 def download_elo_data(date=None) -> None:
     """Note: apparently only European clubs are included."""
+
     if date is None:
         date = datetime.today().strftime('%Y-%m-%d')
     df = pd.read_csv(f"http://api.clubelo.com/{date}")
@@ -34,6 +60,8 @@ def download_elo_data(date=None) -> None:
 
 
 def api_get_leagues() -> None:
+    """Get current leagues from the API and save to a file."""
+
     url = "https://api-football-v1.p.rapidapi.com/v3/leagues"
 
     params = {"current": "true"}
@@ -44,7 +72,8 @@ def api_get_leagues() -> None:
     }
 
     response = requests.get(url, headers=headers, params=params)
-    print(response.status_code)
+    if response.status_code != 200:
+        print(f'Error: {response.status_code}')
 
     if response.json()['paging']['total'] != 1:
         raise Exception("Error: multiple pages of leagues")
@@ -55,6 +84,7 @@ def api_get_leagues() -> None:
 
 
 def api_get_fixtures_for_league(league_id: str, season: str) -> None:
+    """Get fixtures for a given league and season from the API and save to a file."""
     url = "https://api-football-v1.p.rapidapi.com/v3/fixtures"
     params = {"league": league_id, "season": season}
 
@@ -81,29 +111,35 @@ def api_get_standings_for_league() -> None:
 
 
 def find_latest_elo_file() -> str:
+    """Find the latest ELO file in the data/elo directory."""
+
     elo_files = os.listdir("data/elo")
     return f'data/elo/{sorted(elo_files)[-1]}'
 
 
-def get_team_names_from_elo(elo_country_code: str) -> None:
-    df = pd.read_csv(find_latest_elo_file())
-    df = df[(df['Country'] == elo_country_code) & (df['Level'] == 1)]
-    names = sorted(df['Club'].tolist())
-    df = pd.DataFrame(names)
-    df.to_excel('tmp_team_names_elo.xlsx', index=False)
+# def get_team_names_from_elo(elo_country_code: str) -> None:
+#     """Get team names from the latest ELO file for a given country code; 1st league tier."""
+#     df = pd.read_csv(find_latest_elo_file())
+#     df = df[(df['Country'] == elo_country_code) & (df['Level'] == 1)]
+#     names = sorted(df['Club'].tolist())
+#     df = pd.DataFrame(names)
+#     df.to_excel('tmp_team_names_elo.xlsx', index=False)
 
 
-def get_team_names_from_api_dump(path: str) -> None:
-    with open(path, "r") as f:
-        fixtures = json.load(f)
-    names = sorted(
-        set([fixture['teams']['home']['name'] for fixture in fixtures['response']])
-    )
-    df = pd.DataFrame(names)
-    df.to_excel('tmp_team_names_api.xlsx', index=False)
+# def get_team_names_from_api_dump(path: str) -> None:
+#     """Get team names from an API fixtures dump."""
+#     with open(path, "r") as f:
+#         fixtures = json.load(f)
+#     names = sorted(
+#         set([fixture['teams']['home']['name'] for fixture in fixtures['response']])
+#     )
+#     df = pd.DataFrame(names)
+#     df.to_excel('tmp_team_names_api.xlsx', index=False)
 
 
 def find_league_id(country_code: str, league_name: str) -> str:
+    """Find league ID based on country code and league name."""
+
     with open("data/fixtures_api/leagues.json", "r") as f:
         leagues = json.load(f)
         for league in leagues['response']:
@@ -116,46 +152,68 @@ def find_league_id(country_code: str, league_name: str) -> str:
 
 
 def get_api_teams_and_elo_from_clubelo(date: str, country_code: str) -> pd.DataFrame:
+    """Get teams and their ELO ratings from ClubElo data for a given date and country code, 1st league tier."""
 
     date = date.replace('-', '')
     elo_data = pd.read_csv(f"data/elo/{date}.csv")
 
-    elo_data = elo_data[
-        (elo_data['Country'] == country_code) & (elo_data['Level'] == 1)
-    ]
+    if country_code not in ['UCL', 'UEL', 'ECL']:
+        elo_data = elo_data[
+            (elo_data['Country'] == country_code) & (elo_data['Level'] == 1)
+        ]
 
     elo_data['Elo'] = elo_data['Elo'].apply(round)
 
     team_map_df = pd.read_excel('teams_mapping/team_names.xlsx')
 
-    missing_teams = set(elo_data['Club'].tolist()) - set(
-        team_map_df['ELO_name'].tolist()
+    # missing_teams = set(elo_data['Club'].tolist()) - set(
+    #     team_map_df['ELO_name'].tolist()
+    # )
+
+    # if len(missing_teams) > 0:
+    #     print(
+    #         f"The following teams are missing in team_names.xlsx: {missing_teams}. Please add them."
+    #     )
+    #     raise Exception("Missing teams in team_names.xlsx")
+
+    # team_map = {
+    #     row['ELO_name']: row['fixtures_name'] for _, row in team_map_df.iterrows()
+    # }
+
+    # elo_data['Club'] = elo_data['Club'].apply(lambda x: team_map[x])
+
+    # elo_data.reset_index(drop=True, inplace=True)
+
+    # return elo_data[['Club', 'Elo']]
+
+    df = pd.merge(
+        elo_data, team_map_df, left_on='Club', right_on='ELO_name', how='inner'
     )
-    if len(missing_teams) > 0:
-        print(
-            f"The following teams are missing in team_names.xlsx: {missing_teams}. Please add them."
-        )
-        raise Exception("Missing teams in team_names.xlsx")
 
-    team_map = {
-        row['ELO_name']: row['fixtures_name'] for _, row in team_map_df.iterrows()
-    }
+    df = df[['fixtures_name', 'Elo']]
+    df.dropna(inplace=True)
 
-    elo_data['Club'] = elo_data['Club'].apply(lambda x: team_map[x])
+    df.drop_duplicates(inplace=True)
 
-    elo_data.reset_index(drop=True, inplace=True)
+    df.rename(columns={'fixtures_name': 'Club'}, inplace=True)
 
-    return elo_data[['Club', 'Elo']]
+    return df
 
 
-def get_data_from_regression(country_code: str) -> pd.DataFrame:
+def get_data_from_regression(country_code: Optional[str]) -> pd.DataFrame:
+    """Get team names and predicted ELO from regression results, optionally filtered by country code."""
+
     team_map_df = pd.read_excel('teams_mapping/team_names.xlsx')
-    team_map_df = team_map_df[team_map_df['Country_code'] == country_code]
+
+    elo_df = pd.read_csv('data/reg_results.csv')
+
+    if country_code is not None:
+        elo_df = elo_df[elo_df['Country'] == country_code]
+        team_map_df = team_map_df[team_map_df['Country_code'] == country_code]
+
     team_map_df['Opta_name'] = team_map_df['Opta_name'].str.title().str.strip()
     team_map_df = team_map_df[['fixtures_name', 'Opta_name']]
 
-    elo_df = pd.read_csv('data/reg_results.csv')
-    elo_df = elo_df[elo_df['Country'] == country_code]
     elo_df = elo_df[['Opta_name', 'predicted_Elo']]
 
     df = pd.merge(elo_df, team_map_df, on='Opta_name', how='inner')
@@ -175,7 +233,9 @@ def build_historical_standings_table_after_at_most_n_rounds(
     modify_elo: bool = False,
     stdev: Optional[float] = None,
     update_fixtures: Optional[bool] = True,
+    is_european_league: Optional[bool] = False,
 ) -> tuple[pd.DataFrame, pd.DataFrame]:
+    """Build historical standings table after at most n rounds."""
 
     date_str = elo_date.replace('-', '')
     if not os.path.exists(f"data/elo/{date_str}.csv"):
@@ -193,8 +253,7 @@ def build_historical_standings_table_after_at_most_n_rounds(
     if update_fixtures:
         api_get_fixtures_for_league(league_id, season)
 
-    with open(f"data/fixtures_api/fixtures_{league_id}_{season}.json", "r") as f:
-        fixtures = json.load(f)['response']
+    fixtures = read_fixtures(league_id, season, is_european_league)
 
     elo_dict = {row['Club']: row['Elo'] for _, row in elo_df.iterrows()}
     points_dict = {row['Club']: 0 for _, row in elo_df.iterrows()}
@@ -204,9 +263,6 @@ def build_historical_standings_table_after_at_most_n_rounds(
     for fixture in fixtures:
         home_team = fixture['teams']['home']['name']
         fixture_teams.add(home_team)
-
-    # print(fixture_teams)
-    # print(elo_dict.keys())
 
     elo_dict = {k: v for k, v in elo_dict.items() if k in fixture_teams}
     points_dict = {k: v for k, v in points_dict.items() if k in fixture_teams}
@@ -290,12 +346,16 @@ def simulate_season_after_n_rounds(
     league_id: str,
     season: str,
     standings_df: pd.DataFrame,
+    fixtures: Optional[dict] = None,
     reverse: bool = False,
     round_to_overwrite_with_sims_from: int = 999,
     modify_elo_in_sim: bool = False,
+    is_european_league: Optional[bool] = False,
 ) -> pd.DataFrame:
-    with open(f"data/fixtures_api/fixtures_{league_id}_{season}.json", "r") as f:
-        fixtures = json.load(f)['response']
+    """Simulate the rest of the season after n rounds."""
+
+    if fixtures is None:
+        fixtures = read_fixtures(league_id, season, is_european_league)
 
     elo_dict = {row['Club']: row['Elo'] for _, row in standings_df.iterrows()}
     points_dict = {row['Club']: row['Points'] for _, row in standings_df.iterrows()}
@@ -388,7 +448,12 @@ def run_multiple_sims(
     stdev: Optional[float] = None,
     standings_df: Optional[pd.DataFrame] = None,
     update_fixtures: Optional[bool] = True,
+    is_european_league: Optional[bool] = False,
+    range_from: Optional[int] = None,
+    range_to: Optional[int] = None,
 ) -> pd.DataFrame:
+    """Run multiple simulations of the season after n rounds."""
+
     if standings_df is None:
         standings_df = build_historical_standings_table_after_at_most_n_rounds(
             league_id,
@@ -400,12 +465,15 @@ def run_multiple_sims(
             modify_elo_retro,
             stdev,
             update_fixtures,
+            is_european_league,
         )
 
     winners_with_losing_all_tbs = dict()
     winners_with_random_tbs = dict()
 
     xpts = {k: 0 for k in standings_df['Club'].tolist()}
+
+    fixtures = read_fixtures(league_id, season, is_european_league)
 
     for _ in tqdm(range(number_of_sims)):
         new_standings_df = copy.deepcopy(standings_df)
@@ -419,9 +487,11 @@ def run_multiple_sims(
             league_id,
             season,
             new_standings_df,
+            fixtures,
             reverse,
             round_to_overwrite_with_sims_from,
             modify_elo_in_sim,
+            is_european_league,
         )
 
         winners_df['Tiebreaking order'] = np.random.permutation(winners_df.shape[0])
@@ -477,10 +547,16 @@ def run_multiple_sims(
     df.reset_index(drop=True, inplace=True)
 
     df['xPts'] = df['xPts'].apply(lambda x: round(x / number_of_sims, 2))
+    df['RTB Wins'] = df['RTB Wins'].astype(int)
+    df['LTB Wins'] = df['LTB Wins'].astype(int)
     df['% RTB winrate'] = round(df['RTB Wins'] / number_of_sims * 100, 1)
     df['% LTB winrate'] = round(df['LTB Wins'] / number_of_sims * 100, 1)
-    df['Exp. RTB odds'] = round(number_of_sims / df['RTB Wins'], 2)
-    df['Exp. LTB odds'] = round(number_of_sims / df['LTB Wins'], 2)
+    df['Exp. RTB odds'] = df.apply(
+        lambda row: save_round_divide(number_of_sims, row['RTB Wins']), axis=1
+    )
+    df['Exp. LTB odds'] = df.apply(
+        lambda row: save_round_divide(number_of_sims, row['LTB Wins']), axis=1
+    )
     df = df.sort_values(by=['RTB Wins'], ascending=False).reset_index(drop=True)
     df.index += 1
     print(f'{number_of_sims} simulations')
@@ -506,8 +582,124 @@ def run_full_table_sims(
     stdev: Optional[float] = None,
     standings_df: Optional[pd.DataFrame] = None,
     update_fixtures: Optional[bool] = True,
+    is_european_league: Optional[bool] = False,
 ) -> pd.DataFrame:
+    """Run full table simulations of the season after n rounds."""
 
-    # TODO
+    if standings_df is None:
+        standings_df = build_historical_standings_table_after_at_most_n_rounds(
+            league_id,
+            season,
+            country_code_elo,
+            country_code_api,
+            elo_date,
+            last_round_for_standings,
+            modify_elo_retro,
+            stdev,
+            update_fixtures,
+            is_european_league,
+        )
 
-    pass
+    xpts = {k: 0 for k in standings_df['Club'].tolist()}
+
+    main_results_df = standings_df[['Club', 'Elo']].copy()
+    main_results_df.set_index('Club', inplace=True)
+    for place in range(1, standings_df.shape[0] + 1):
+        main_results_df[place] = 0
+
+    fixtures = read_fixtures(league_id, season, is_european_league)
+
+    for _ in tqdm(range(number_of_sims)):
+        new_standings_df = copy.deepcopy(standings_df)
+
+        if stdev is not None and stdev != 0:
+            new_standings_df['Elo'] = new_standings_df['Elo'] + [
+                round(random.gauss(0, stdev)) for _ in range(new_standings_df.shape[0])
+            ]
+
+        winners_df = simulate_season_after_n_rounds(
+            league_id,
+            season,
+            new_standings_df,
+            fixtures,
+            reverse,
+            round_to_overwrite_with_sims_from,
+            modify_elo_in_sim,
+            is_european_league,
+        )
+
+        winners_df['Tiebreaking order'] = np.random.permutation(winners_df.shape[0])
+
+        winners_df = winners_df.sort_values(
+            by=['Points', 'Tiebreaking order'], ascending=reverse
+        ).reset_index(drop=True)
+
+        for i in range(winners_df.shape[0]):
+            club = winners_df.iloc[i]['Club']
+            main_results_df.at[club, i + 1] += 1
+            points = winners_df.iloc[i]['Points']
+            xpts[club] += points
+
+    xpts_df = pd.DataFrame(list(xpts.items()), columns=['Club', 'Expected Points'])
+    df = pd.merge(main_results_df, xpts_df, on='Club', how='inner')
+    df.rename(columns={'Expected Points': 'xPts'}, inplace=True)
+
+    df.fillna(0, inplace=True)
+
+    df['xPts'] = df['xPts'].apply(lambda x: round(x / number_of_sims, 2))
+
+    if is_european_league:
+        df['Top 8'] = df.apply(
+            lambda row: round(
+                sum([row[i] for i in range(1, 9)]) / number_of_sims * 100, 1
+            ),
+            axis=1,
+        )
+        df['9 - 24'] = df.apply(
+            lambda row: round(
+                sum([row[i] for i in range(9, 25)]) / number_of_sims * 100, 1
+            ),
+            axis=1,
+        )
+    else:
+        df['Top 4'] = df.apply(
+            lambda row: round(
+                sum([row[i] for i in range(1, 5)]) / number_of_sims * 100, 1
+            ),
+            axis=1,
+        )
+
+    for place in range(1, standings_df.shape[0] + 1):
+        df[place] = df[place].apply(lambda x: round(x / number_of_sims * 100, 1))
+
+    df.sort_values(by=['xPts'], ascending=False, inplace=True)
+
+    if is_european_league:
+        df = df[
+            ['Club', 'Elo', 'xPts', 'Top 8', '9 - 24']
+            + list(range(1, standings_df.shape[0] + 1))
+        ]
+    else:
+        df = df[
+            ['Club', 'Elo', 'xPts', 'Top 4'] + list(range(1, standings_df.shape[0] + 1))
+        ]
+
+    df.reset_index(drop=True, inplace=True)
+    df.index += 1
+    print(f'{number_of_sims} simulations')
+    print(f'{number_of_winning_places} winning places')
+    if reverse:
+        print('Reverse: TRUE')
+    Path('data/sims').mkdir(parents=True, exist_ok=True)
+    df.to_excel(f'data/sims/{league_id}_{season}_full_table_sim.xlsx', index=False)
+    return df
+
+
+# standings_df = build_historical_standings_table_after_at_most_n_rounds(
+#     league_id=109,
+#     season=2025,
+#     country_code_elo=None,
+#     country_code_api='POL',
+#     elo_date='2025-11-10',
+#     update_fixtures=False,
+# )
