3ac33707c297434030357ae26bbad28feb5f3871
MZ <zablockimikael@gmail.com>
2026-02-21 13:03:10 +0100

    1.4

--- TODO.py
+++ TODO.py
@@ -2,32 +2,23 @@
 
 # high prio
 
-# 0. just create a linear regression with extra random features since this just won't work (ML)
-# 1. compare "own" odds to bookmaker odds for current games
-# 2. then simulate both bookmakers elo => bookmakers outrights and vice versa
-# 3. add III Polish league (update teams mapping)
+# 1. assume each team is 2 STDs stronger/weaker; std number of goals as well
+# 2. refactor run_full_table_sims; add odds calculation (inverse probs)
+# 3. also check how much disparity there is bettween the top and bottom teams in a league as an extra verification
+# 4. table with odds for each of the games in the league
+# 5. compare which of two teams will finish higher in the league [can be a table as well]
 
 ###
 
 # medium prio
 
 # 1. run an analysis like in the paper - middle of season standings vs. end of season standings
-# 2. past futures.xlsx - transform to something sensible
+# 2. past futures.xlsx - transform to something sensible / analyze
 # 3. graph how changing one team's elo affects the outright [predicted] odds
-# 4. add WTB (winning tie-breakers) sim
-# 5. table with odds for each of games in the league
-# 6. compare which of two teams will finish higher in the league
-# 7. check changes of teams between leagues
-
-###
-
-# low prio
-
-# 1. get/scrape outrights odds [check the odds api] OR parse the screenshots
-# 2. scrape opta power rankings
-# 3. [low prio] visualization: conditional formatting of odds, graph odds over time, frontend
-# 4. add a function which of the two teams will finish higher
-# 5. set HFA to 90 for international competitions
+# 4. check changes of teams between leagues
+# 5. write a script for parsing the screenshots with odds
+# 6. modify HFA for international competitions
+# 7. code points deductions better(?)
 
 ###
 
